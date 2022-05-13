feb0a87de0712e85c68b6e1ea2548a045fd36e4d
Agustin <agustin.derobles1995@gmail.com>
2022-05-12 21:13:24 -0300

    ejemplos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
-function App() {
+/*function App() {
+
+  const [tipodata, setTipodata] = useState('posts')
+
+  useEffect(() => {
+    console.log('data change')
+    
+    return ()=> {
+      console.log('data Return')
+    }
+  }, [tipodata])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Holaaaaa</h1>
-      </header>
+      <div>
+        <button  onClick={() => setTipodata('posts') }> Posts </button>
+        <button  onClick={() => setTipodata('users') }> Users </button>
+        <button  onClick={() => setTipodata('comments') }> Comments </button>
+      </div>
+      <h3>{ tipodata }</h3>
+      
     </div>
   );
+}*/
+
+function App(){
+  const [ancho, setAncho] = useState(window.innerWidth);
+
+  const cambtamanio = () => {
+    setAncho(window.innerWidth);
+  };
+  
+
+  useEffect(() => {
+    window.addEventListener("resize", cambtamanio);
+  },[ancho]);
+
+  return(
+    <div>
+      <h1> {ancho} </h1>
+    </div>
+  ) 
 }
 
 export default App;
